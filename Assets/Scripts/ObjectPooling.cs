4f29e3a1029cbbff903960f5d2c62f41e2e2b8cc
Luminous3515 <taeun3515@gmail.com>
2024-11-20 18:32:02 +0900

    241120 오브젝트 풀링
    1. 오브젝트 풀링
    2. 행성 프리팹으로 변경

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    // 행성들 트랜스폼
+    [SerializeField] Transform[] planetsTr;
+
+    // 플레이어 트랜스폼
+    [SerializeField] Transform playerTr;
+
+    // 행성 인덱스 > 순서대로 가져옴
+    private int planetInedx;
+
+    #region// 랜덤생성거리 Min ~ Max X, Y 생성
+    // 랜덤으로 스폰하는 위치 최대와 최소
+    private float randomSpawnMinX;
+    private float randomSpawnMaxX;
+
+    // 플레이어 위치에 더하기 Y
+    private float randomSpawnMinY;
+    private float randomSpawnMaxY;
+    #endregion
+
+    // 최종 스폰 위치
+    private Vector2 randomSpawnPos;
+
+    private void Start()
+    {
+        // 값 할당
+
+        #region// 랜덤생성거리 Min ~ Max X, Y 할당
+        randomSpawnMinX = -7.34f;
+        randomSpawnMaxX = 7.34f;
+
+        randomSpawnMinY = 1.25f;
+        randomSpawnMaxY = 4;
+        #endregion
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            // 랜덤 스폰 위치 할당 + 플레이어 위치
+            randomSpawnPos = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y);
+
+            planetsTr[i].position = randomSpawnPos;
+
+            planetInedx++;
+        }
+    }
+
+
+    // 행성 풀링
+    public void PlanetsPooling()
+    {
+
+        // 만약 행성의 갯수랑 인덱스랑 숫자가 같다면 첫번째 행성을 가져오는 코드
+        if (planetInedx == planetsTr.Length)
+        {
+            planetInedx = 0;
+        }
+
+        // 스폰하는 최종 위치 
+        randomSpawnPos = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), (Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y));
+
+        //Debug.Log(randomSpawnPos);        
+
+        // 행성 위치를 이동
+        planetsTr[planetInedx].position = randomSpawnPos;
+
+        Debug.Log(planetInedx);       
+
+        // 인덱스 증감
+        planetInedx++;
+
+
+    }
+
+}
---
+++ Assets/Scripts/StandbyPos.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class StandbyPos : MonoBehaviour
+{
+    // 풀링 스크립트
+    [SerializeField] ObjectPooling objectPooling;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // 태그가 Planet이라면 PlanetsPooling호출 및 행성 대기위치로 이동
+        if (collision.CompareTag("Planet"))
+        {
+            objectPooling.PlanetsPooling();
+
+        }
+
+
+    }    
+
+}
