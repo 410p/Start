2ce789b27d9d466d7d2a73894f392bb9be255723
Luminous3515 <taeun3515@gmail.com>
2024-11-25 23:12:36 +0900

    [New] 빔 쏘는 적 구현
    1. 랜덤 위치로 이동
    2. 빔 발사 구현(임시로 W 누르면 해당 객체를 생성할 수 있습니다.)
    3. 빔이 커지기 전에는 위험구역이 보이고 축소할 때 위험구역이 사라집니다.
    4. 빔 스프라이트에 피벗을 위(Top)으로 옮김
    
    *주의사항*
    빔 쏘는 객체는 하이어라키에 총 6개이므로 6개 이상을 동시에 소환하면 인덱스 오류가 걸린다. 그러므로 더 많은 객체가 필요할 때는 객체를 추가해야 한다.

--- Assets/Scripts/BeamEnemy.cs
+++ Assets/Scripts/BeamEnemy.cs
@@ -1,117 +1,110 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class BeamEnemy : MonoBehaviour
-{    
-    // 이동 속도
-    private float moveSpeed;
-
-    // 적의 체력
-    private int enemyHP;
-
-    // 적의 움직임
-    private Vector2 movement;
-
-    // 적의 움직일 방향
-    private float moveDirection;
-
+{
     // 대기위치
-    private Vector2 StandbyPos;
+    private Vector2 StandbyPos;      
 
-    // 사용중인지?
-    private bool inUse;
-    public bool InUse => inUse;
+    // 적이 발사할 빔
+    [SerializeField] Sprite[] beams;
 
-    // 빔 쏘는 적의 리지드바디
-    private Rigidbody2D enemy_BeamRb;
+    // 발사를 몇 번 했는지 확인하는 변수
+    private int beamsIndex;
 
-    // 초반 오른쪽으로 움직일지 왼쪽으로 움직일지 정하는 랜덤 변수
-    private int randomDirection;
+    // 빔을 발사할 위치
+    [SerializeField] GameObject beamFirePos;
+
+    // 빔을 처음 쏘기까지 걸리는 대기시간
+    private WaitForSeconds beamFirstFireTime;
+
+    // 빔 스프라이트 변경할 대기시간(최적화)
+    private WaitForSeconds beamDelay;
+
+    // 빔 스프라이트를 다시 제거하는 시간(최적화)
+    private WaitForSeconds beamDestroyDelay;
+
+    // 빔의 공격 사거리
+    [SerializeField] GameObject Beam_intersection;
     private void Start()
     {
-        // 할당        
+        // 할당          
 
-        enemy_BeamRb = GetComponent<Rigidbody2D>();
+        beamsIndex = 0;
 
-        moveSpeed = 1000f;
+        beamDelay = new WaitForSeconds(0.05f);
 
-        inUse = false;
+        beamDestroyDelay = new WaitForSeconds(0.1f);
 
-        // 초반 오른쪽으로 움직일지 왼쪽으로 움직일지 정하는 코드
-        randomDirection = Random.Range(0, 2);
-        //Debug.Log(randomDirection);
-
-        // 방향을 오른쪽 으로 설정
-        if (randomDirection == 0) moveDirection = 1;
-
-        // 방향을 왼쪽 으로 설정
-        else moveDirection = -1;
-
-        // 임시
-        enemyHP = 5;
+        beamFirstFireTime = new WaitForSeconds(1f);
 
         // 대기 위치 정의
-        StandbyPos = new Vector2(32.45f, 0.83f);        
-    }
-
-    // 적의 체력
-    public void EnemyHp()
-    {
-        if (!inUse) return;
-
-        enemyHP--;
-
-        // 죽는다면
-        if (enemyHP <= 0)
-        {
-            // 세팅 함수 호출
-            Setting();
-        }
+        StandbyPos = new Vector2(32.45f, 0.83f);
     }
 
     // 오브젝트 능력치 초기화
-    private void Setting()
-    {
-        // 사용중이지 않음
-        inUse = false;
-
+    private void OffSetting()
+    {             
         // 대기위치로 이동            
         transform.position = StandbyPos;
-
-        // 값 초기화
-        enemyHP = 5;
-
-
     }
 
-    // 논리충돌 감지
-    private void OnTriggerEnter2D(Collider2D collision)
+    // 오브젝트 위치 설정
+    public void OnSetting(Vector2 pos)
     {
-        if (collision.CompareTag("Right Collider"))
-        {
-            // 방향을 왼쪽 으로 설정
-            moveDirection = -1;
-        }
-        else if (collision.CompareTag("Left Collider"))
-        {
-            // 방향을 오른쪽 으로 설정
-            moveDirection = 1;
-        }
+        gameObject.transform.position = pos;        
+
+        StartCoroutine(FireBeam());
     }
 
-    // 빔 쏘는 적 움직임
-    private IEnumerator Movement()
+    
+
+    // 빔을 발사하는 함수
+    private IEnumerator FireBeam()
     {
-        while (true)
+        // 위험 표시 생성
+        Beam_intersection.SetActive(true);
+
+        // 처음 발사까지 걸리는 시간
+        yield return beamFirstFireTime; 
+
+        while (beamsIndex < beams.Length)
         {
-            // 오브젝트를 사용중이지 않다면 작동 X
-            if (!inUse) break;
 
-            // 움직임 정의
-            movement = new Vector2(moveDirection, 0 * moveSpeed);
-            enemy_BeamRb.velocity = movement;
+            beamFirePos.GetComponent<SpriteRenderer>().sprite = beams[beamsIndex];
 
-            yield return null;
+            beamsIndex++;
+
+            yield return beamDelay;
+
+
         }
+
+        StartCoroutine(DestroyBeam());
+    }
+
+    // 빔을 줄이는 함수
+    private IEnumerator DestroyBeam()
+    {
+        // 위험 표시 제거
+        Beam_intersection.SetActive(false);
+
+        // 인덱스기 때문에 한번 감소
+        beamsIndex--;
+
+        while (beamsIndex > 0)
+        {
+            //Debug.Log(beamsIndex);
+
+            beamFirePos.GetComponent<SpriteRenderer>().sprite = beams[beamsIndex];
+
+            beamsIndex--;
+
+            yield return beamDestroyDelay;
+        }
+
+        OffSetting();
+
     }
 }
--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -5,45 +5,29 @@ using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
-    
-    // 행성들 트랜스폼 (초반에만 사용할 변수)
-    [SerializeField] Transform[] planetsTr;
 
-    //// 가스형 행성들 트랜스폼
-    //[SerializeField] Transform[] planetsTr_Gas;
 
-    // 플레이어 트랜스폼
-    [SerializeField] Transform playerTr;
-
-    #region// 랜덤생성거리 Min ~ Max X, Y 생성
     // 랜덤으로 스폰하는 위치 최대와 최소
     private float randomSpawnMinX;
     private float randomSpawnMaxX;
 
-    // 플레이어 위치에 더하기 Y
+
     private float randomSpawnMinY;
     private float randomSpawnMaxY;
 
-    // 최종 스폰 위치
-    private Vector2 randomSpawnPos;
-    #endregion
-
-    // 스폰 가능한지
-    private bool returnSpawn = false;
-    public bool ReturnSpawn { set { returnSpawn = value; } }
+    // 플레이어 트랜스폼
+    [SerializeField] Transform playerTr;
 
     private void Awake()
     {
-        // 값 할당               
-
-        #region// 랜덤생성거리 Min ~ Max X, Y 할당
+        // 값 할당           
 
         randomSpawnMinX = -7.34f;
         randomSpawnMaxX = 7.34f;
 
         randomSpawnMinY = 6.19f;
-        randomSpawnMaxY = 17;
-        #endregion
+        randomSpawnMaxY = 15;
+
     }
 
     private void Start()
@@ -51,17 +35,38 @@ public class ObjectPooling : MonoBehaviour
         // 시작할 때 한 번 추가
         for (int i = 0; i < 22; i++)
         {
-            randomSpawnPos = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), (Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y + i));
+            randomSpawnPos_Planet = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), (Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y + i));
 
 
             // 행성 위치를 이동
-            planetsTr[i].position = randomSpawnPos;
+            planetsTr[i].position = randomSpawnPos_Planet;
 
 
 
+        }        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnBeamEnemy();
         }
     }
 
+
+    #region// 일반행성 생성
+
+    // 행성들 트랜스폼 (초반에만 사용할 변수)
+    [SerializeField] Transform[] planetsTr;
+
+    // 스폰 가능한지
+    private bool returnSpawn = false;
+
+    // 일반행성 최종 스폰위치
+    private Vector2 randomSpawnPos_Planet;
+
+    public bool ReturnSpawn { set { returnSpawn = value; } }
 
     // 일반행성 풀링 (매개변수 : planet : 어떤 행성을 다시 사용할 것 인가?)
     public void PlanetsPooling(GameObject planet)
@@ -70,13 +75,41 @@ public class ObjectPooling : MonoBehaviour
         if (returnSpawn) return;
 
         // 랜덤 위치 뽑기
-        randomSpawnPos = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), (Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y));
+        randomSpawnPos_Planet = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), (Random.Range(randomSpawnMinY, randomSpawnMaxY) + playerTr.position.y));
 
         //Debug.Log(randomSpawnPos);        
 
         //Debug.Log(planetInedx);
 
         // 매개변수로 넘어온 행성 위치를 이동
-        planet.transform.position = randomSpawnPos;
+        planet.transform.position = randomSpawnPos_Planet;
     }
+    #endregion
+
+    #region// 적 생성
+
+    // 빔 쏘는 적 배열
+    [SerializeField] GameObject[] beamEnemy;
+
+    // 빔 쏘는 적의 최종 스폰 위치
+    private Vector2 randomSpawnPos_BeamEnemy;
+
+    // 빔 쏘는 적 차례대로 생성
+    private int beamEnemyIndex;
+
+    // 빔 쏘는 적 생성
+    private void SpawnBeamEnemy()
+    {
+        if (beamEnemyIndex >= beamEnemy.Length) beamEnemyIndex = 0;
+
+        randomSpawnPos_BeamEnemy = new Vector2(Random.Range(randomSpawnMinX, randomSpawnMaxX), playerTr.position.y + 7.5f);
+
+        //Debug.Log(beamEnemyIndex);
+
+        beamEnemy[beamEnemyIndex].SetActive(true);
+        beamEnemy[beamEnemyIndex].GetComponent<BeamEnemy>().OnSetting(randomSpawnPos_BeamEnemy);
+
+        beamEnemyIndex++;
+    }
+    #endregion
 }
